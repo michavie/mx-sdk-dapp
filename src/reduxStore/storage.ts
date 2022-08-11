17205cda956d17d8233b7d65b7b352207b6f7868
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-08-11 18:57:08 +0300

    custom storage

--- src/reduxStore/storage.ts
+++ src/reduxStore/storage.ts
@@ -1,15 +1,26 @@
 import reduxPersistLocalStorage from 'redux-persist/lib/storage';
 import reduxPersistSessionStorage from 'redux-persist/lib/storage/session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Storage } from 'redux-persist';
+
+const isMobileDevice = true;
+// platform?.os?.family === 'iOS' || platform?.os?.family === 'Android';
+
+console.log('isMobileDevice = ', isMobileDevice);
 
 let persistorLocalStorage: Storage = reduxPersistLocalStorage;
 let persistorSessionStorage: Storage = reduxPersistSessionStorage;
 
-export const getPersistorLocalStorage = () => persistorLocalStorage;
-export const getPersistorSessionStorage = () => persistorSessionStorage;
+export const getPersistorLocalStorage = () =>
+  isMobileDevice ? AsyncStorage : persistorLocalStorage;
+
+export const getPersistorSessionStorage = () =>
+  isMobileDevice ? AsyncStorage : persistorSessionStorage;
+
 export const setPersistorLocalStorage = (storage: Storage) => {
   persistorLocalStorage = storage;
 };
+
 export const setPersistorSessionStorage = (storage: Storage) => {
   persistorSessionStorage = storage;
 };
