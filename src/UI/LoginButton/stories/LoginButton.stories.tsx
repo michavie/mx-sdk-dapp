23510512621892131926089c4945e105e53ca9a2
Tudor Morar <tudor.morar@elrond.com>
2022-12-06 13:23:57 +0300

    Storybook interaction tests runnning

--- src/UI/LoginButton/stories/LoginButton.stories.tsx
+++ src/UI/LoginButton/stories/LoginButton.stories.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
+import { expect, jest } from '@storybook/jest';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { LoginButton } from './../LoginButton';
+import { within, fireEvent } from '@storybook/testing-library';
+import { LoginButton as LoginBtn } from './../LoginButton';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'UI/LoginButton',
-  component: LoginButton,
+  component: LoginBtn,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' }
   }
-} as ComponentMeta<typeof LoginButton>;
+} as ComponentMeta<typeof LoginBtn>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof LoginButton> = (args) => (
-  <LoginButton {...args} />
-);
+const Template: ComponentStory<typeof LoginBtn> = (args) => {
+  return <LoginBtn {...args} />;
+};
 
-export const Text = Template.bind({});
+export const LoginButton = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Text.args = {
-  text: 'Login basic'
+
+// const onClick = jest.fn();
+
+const buttonDataTestId = 'loginButton';
+
+LoginButton.args = {
+  text: 'Login basic',
+  'data-testid': buttonDataTestId,
+  onLogin: jest.fn()
+};
+
+LoginButton.parameters = { ...Template.parameters };
+LoginButton.play = async ({ canvasElement, args }) => {
+  const canvas = within(canvasElement);
+  const loginButton = await canvas.findByTestId(String(args['data-testid']));
+  expect(args.onLogin).toHaveBeenCalledTimes(0);
+
+  fireEvent.click(loginButton);
+  expect(args.onLogin).toHaveBeenCalledTimes(1);
 };
